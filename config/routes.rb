e89bec467256f966b25d95e5e4d23d7621bedcb7
Carliff <carliffrizal.carleel@gmail.com>
2016-01-15 15:56:22 +0800

    Added Devise and User time zone

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :users, controller: { registrations: 'registrations'}
+  get 'pages/home'
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
